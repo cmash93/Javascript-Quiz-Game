28b86e1ee0a6ebd3eae7eb6f838992dd68ff8de6
cmash93 <cmash93@gmail.com>
2022-08-25 04:45:03 -0500

    FINALLY got questions randomized and showing up properly

--- assets/JS/questions.js
+++ assets/JS/questions.js
@@ -3,45 +3,45 @@ var questionsArray = [
     {
         question: 'Which of the following instructions is correct to change the background color of the page?',
         answersArray: [
-            {choice1: 'document.body.background = "red"', correct: false},
-            {choice2: 'document.body.color = "red"', correct: false},
-            {choice3: 'document.body.style.background = "red"', correct: true}, // Correct
-            {choice4: 'document.body.style.color = "red"', correct: false}
+            {choice: 'document.body.background = "red"', correct: false},
+            {choice: 'document.body.color = "red"', correct: false},
+            {choice: 'document.body.style.background = "red"', correct: true}, // Correct
+            {choice: 'document.body.style.color = "red"', correct: false}
         ]
     },
 
     {
         question: 'How do you link an external Javascript file in HTML?',
         answersArray: [
-            {choice1: '<script src="filename.js"></script>', correct: true}, // Correct
-            {choice2: '<script rel="filename.js"></script>', correct: false},
-            {choice3: '<script url="filename.js"></script>', correct: false},
-            {choice4: '<script href="filename.js"></script>', correct: false}
+            {choice: '<script src="filename.js"></script>', correct: true}, // Correct
+            {choice: '<script rel="filename.js"></script>', correct: false},
+            {choice: '<script url="filename.js"></script>', correct: false},
+            {choice: '<script href="filename.js"></script>', correct: false}
         ]
     },
 
     {
         question: 'To get a value from user input, we use which box?',
         answersArray: [
-            {choice1: 'confirm', correct: false},
-            {choice2: 'prompt', correct: true}, //Correct
-            {choice3: 'alert', correct: false},
-            {choice4: 'None of the above', correct: false}
+            {choice: 'confirm', correct: false},
+            {choice: 'prompt', correct: true}, //Correct
+            {choice: 'alert', correct: false},
+            {choice: 'None of the above', correct: false}
         ]
     },
 
     {
         question: 'What does JSON stand for?',
         answersArray: [
-            {choice1: 'JavaScript Oriented Notation', correct: false},
-            {choice2: 'JavaScript Oreo Notation', correct: false},
-            {choice3: 'JavaScript Online Notation', correct: false},
-            {choice4: 'JavaScript Object Notation', correct: true} // Correct
+            {choice: 'JavaScript Oriented Notation', correct: false},
+            {choice: 'JavaScript Oreo Notation', correct: false},
+            {choice: 'JavaScript Online Notation', correct: false},
+            {choice: 'JavaScript Object Notation', correct: true} // Correct
         ]
     }
 ]
 
-function getNewQuestion() {
+function randomizeQuestions() {
     let questionsClone = questionsArray;
     for (let i = 0; i < questionsClone.length; i++) {
 
@@ -51,9 +51,9 @@ function getNewQuestion() {
         });
     }
 
-    questionsArray = questionsArray.sort(() => {
+    questionsClone = questionsClone.sort(() => {
         return (Math.floor(Math.random() * 3) - 1);
     });
 
-    return questionsArray;
+    return questionsClone;
 }
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,17 +1,59 @@
 var startButton = document.querySelector('#start');
 var timer = document.querySelector('.timerCounter');
-var question = document.querySelector('.question');
-var choices = Array.from(document.querySelector('.choice-text'));
+var question = document.querySelector('#question');
 var highScores = document.querySelector('#high-scores');
 var finalScoreText = document.querySelector('.finalScore');
 var initials = document.querySelector('#initials')
+var shuffledQuestions = randomizeQuestions();
 
 
 var secondsLeft = 60;
 var currentScore = 0;
-var currentQuestion = -1;
+var currentQuestion = 0;
 var finalScore;
 
+var questionsArray = [
+
+    {
+        question: 'Which of the following instructions is correct to change the background color of the page?',
+        answersArray: [
+            {choice: 'document.body.background = "red"', correct: false},
+            {choice: 'document.body.color = "red"', correct: false},
+            {choice: 'document.body.style.background = "red"', correct: true}, // Correct
+            {choice: 'document.body.style.color = "red"', correct: false}
+        ]
+    },
+
+    {
+        question: 'How do you link an external Javascript file in HTML?',
+        answersArray: [
+            {choice: '<script src="filename.js"></script>', correct: true}, // Correct
+            {choice: '<script rel="filename.js"></script>', correct: false},
+            {choice: '<script url="filename.js"></script>', correct: false},
+            {choice: '<script href="filename.js"></script>', correct: false}
+        ]
+    },
+
+    {
+        question: 'To get a value from user input, we use which box?',
+        answersArray: [
+            {choice: 'confirm', correct: false},
+            {choice: 'prompt', correct: true}, //Correct
+            {choice: 'alert', correct: false},
+            {choice: 'None of the above', correct: false}
+        ]
+    },
+
+    {
+        question: 'What does JSON stand for?',
+        answersArray: [
+            {choice: 'JavaScript Oriented Notation', correct: false},
+            {choice: 'JavaScript Oreo Notation', correct: false},
+            {choice: 'JavaScript Online Notation', correct: false},
+            {choice: 'JavaScript Object Notation', correct: true} // Correct
+        ]
+    }
+]
 
 // First, functions for the first page!
 // The start button will need to take the user to the quiz.
@@ -20,7 +62,7 @@ function startQuiz() {
     console.log('Started');
     document.getElementById('home').style.display="none";
     document.getElementById('game').style.display='block';
-
+    
     startTimer();
     getNewQuestion();
 }
@@ -38,13 +80,28 @@ function startTimer() {
     }, 100)
 }
 
+var choiceOne = document.querySelector('#choice-1')
+var choiceTwo = document.querySelector('#choice-2')
+var choiceThree = document.querySelector('#choice-3')
+var choiceFour = document.querySelector('#choice-4')
+
 function getNewQuestion() {
-    currentQuestion
-}
-
-// function selectAnswer() {
-
-// }
+    currentQuestion++;
+    if (currentQuestion === shuffledQuestions.length) {
+        secondsLeft = 0;
+        endGame();
+    }
+    else {
+        question.textContent = shuffledQuestions[currentQuestion].question;
+        
+        let choices = [choiceOne, choiceTwo, choiceThree, choiceFour];
+        let i = 0;
+        choices.forEach(element => {
+            element.textContent = shuffledQuestions[currentQuestion].answersArray[i].choice;
+            i++
+        }, i);
+    };
+};
 
 
 
