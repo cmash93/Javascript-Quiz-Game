69ba3009406da1caf83a71cb3c4f1ef5baa58d30
cmash93 <cmash93@gmail.com>
2022-09-29 14:38:32 -0500

    html setup differently

--- assets/JS/questions.js
+++ assets/JS/questions.js
@@ -1,42 +1,60 @@
-var questions = [
-    {
-      questionText: 'Which of the following instructions is correct to change the background color of the page?',
-      multiChoice: [
-        'document.body.background = "red"', 
-        'document.body.color = "red"', 
-        'document.body.style.background = "red"', 
-        'document.body.style.color = "red"'
-    ],
-      answer: 'document.body.style.background = "red"'
-    },
-    {
-        questionText: 'How do you link an external Javascript file in HTML?',
-        multiChoice: [
-          '<script src="filename.js"></script>', 
-          '<script rel="filename.js"></script>', 
-          '<script url="filename.js"></script>', 
-          '<script href="filename.js"></script>'
-        ],
-        answer: '<script src="filename.js"></script>'
-      },
-      {
-        questionText: 'Which of the following instructions is correct to change the background color of the page?',
-        multiChoice: [
-          'confirm', 
-          'prompt', 
-          'alert', 
-          'box'
-        ],
-        answer: 'prompt'
-      },
-      {
-        questionText: 'What does JSON stand for?',
-        multiChoice: [
-          'JavaScript Oriented Notation', 
-          'JavaScript Oreo Notation', 
-          'JavaScript Online Notation', 
-          'JavaScript Object Notation'
-        ],
-        answer: 'JavaScript Object Notation'
-      },
-  ];
+let questions = [
+  {
+    num: 1,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    num: 2,
+    question: 'Which of the following instructions is correct to change the background color of the page?',
+    answer: 'document.body.style.background = "red"',
+    options: [
+      'document.body.background = "red"', 
+      'document.body.color = "red"', 
+      'document.body.style.background = "red"', 
+      'document.body.style.color = "red"'
+    ]
+  },
+
+  {
+    num: 3,
+    question: 'How do you link an external Javascript file in HTML?',
+    answer: '<script src="filename.js"></script>',
+    options: [
+      '<script src="filename.js"></script>', 
+      '<script rel="filename.js"></script>', 
+      '<script url="filename.js"></script>', 
+      '<script href="filename.js"></script>'
+    ]
+  },
+
+  {
+    num: 4,
+    question: 'Which of the following instructions is correct to change the background color of the page?',
+    answer: 'prompt',
+    options: [
+      'confirm', 
+      'prompt', 
+      'alert', 
+      'box'
+    ]
+  },
+
+  {
+    num: 5,
+    questions: 'What does JSON stand for?',
+    answer: 'JavaScript Object Notation',
+    options: [
+      'JavaScript Oriented Notation', 
+      'JavaScript Oreo Notation', 
+      'JavaScript Online Notation', 
+      'JavaScript Object Notation'
+    ]
+  },
+];
